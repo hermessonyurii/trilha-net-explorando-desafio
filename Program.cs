56ad17b5938cd65ae20438ebad1e8459ce91cb4e
Hermessonyurii <hermessonyuri@hotmail.com>
2025-09-21 17:34:17 -0300

    Corrigi o projeto, deixando funcional, e acrescentei um Menu!

--- Program.cs
+++ Program.cs
@@ -1,25 +1,91 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using DesafioProjetoHospedagem.Models;
 
-Console.OutputEncoding = Encoding.UTF8;
+class Program
+{
+    static void Main()
+    {
+        Reserva reserva = new Reserva();
+        bool sair = false;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+        while (!sair)
+        {
+            Console.WriteLine("==== Sistema de Reserva de Hotel ====");
+            Console.WriteLine("1. Cadastrar Suíte");
+            Console.WriteLine("2. Cadastrar Hóspedes");
+            Console.WriteLine("3. Definir Dias Reservados");
+            Console.WriteLine("4. Mostrar Quantidade de Hóspedes");
+            Console.WriteLine("5. Calcular Valor da Diária");
+            Console.WriteLine("6. Sair");
+            Console.Write("Escolha uma opção: ");
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("Tipo da suíte: ");
+                    string tipo = Console.ReadLine();
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+                    Console.Write("Capacidade: ");
+                    int capacidade = int.Parse(Console.ReadLine());
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+                    Console.Write("Valor da diária: ");
+                    decimal valorDiaria = decimal.Parse(Console.ReadLine());
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+                    Suite suite = new Suite(tipo, capacidade, valorDiaria);
+                    reserva.CadastrarSuite(suite);
+                    Console.WriteLine("Suíte cadastrada com sucesso!\n");
+                    break;
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+                case "2":
+                    Console.Write("Quantos hóspedes deseja cadastrar? ");
+                    int qtd = int.Parse(Console.ReadLine());
+
+                    var hospedes = new List<Pessoa>();
+                    for (int i = 0; i < qtd; i++)
+                    {
+                        Console.Write($"Nome do hóspede {i + 1}: ");
+                        string nome = Console.ReadLine();
+                        Console.Write($"Sobrenome do hóspede {i + 1}: ");
+                        string sobrenome = Console.ReadLine();
+                        hospedes.Add(new Pessoa(nome, sobrenome));
+                    }
+
+                    try
+                    {
+                        reserva.CadastrarHospedes(hospedes);
+                        Console.WriteLine("Hóspedes cadastrados com sucesso!\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}\n");
+                    }
+                    break;
+
+                case "3":
+                    Console.Write("Digite o número de dias reservados: ");
+                    reserva.DiasReservados = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Dias reservados atualizados!\n");
+                    break;
+
+                case "4":
+                    Console.WriteLine($"Quantidade de hóspedes: {reserva.ObterQuantidadeHospedes()}\n");
+                    break;
+
+                case "5":
+                    decimal valorTotal = reserva.CalcularValorDiaria();
+                    Console.WriteLine($"Valor total da diária: R$ {valorTotal}\n");
+                    break;
+
+                case "6":
+                    sair = true;
+                    Console.WriteLine("Saindo do programa. Até mais!");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida! Tente novamente.\n");
+                    break;
+            }
+        }
+    }
+}
